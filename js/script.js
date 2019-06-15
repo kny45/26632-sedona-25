4c172984e420a73f46cde8e69f722c9128f24bb6
kny45 <kny.komarov@gmail.com>
2019-06-15 18:52:27 +0300

    Update script.js
    Внесены корректировки по итогам проверки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var searchButton = document.querySelector(".hotel-search-button");
 var popup = document.querySelector(".modal-hotel-search");
 
+popup.classList.toggle("modal-hotel-search-close");
+
 searchButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-hotel-search-close");
